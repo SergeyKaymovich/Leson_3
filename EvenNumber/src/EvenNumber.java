88372077e13443aa9b546a89a5552f8a1086ca47
Sergey Kaymovich <Sergey.kaymovich@mail.ru>
2022-12-27 23:01:10 +0300

    Done Lesson 3.

--- EvenNumber/src/EvenNumber.java
+++ EvenNumber/src/EvenNumber.java
@@ -5,8 +5,11 @@ public class EvenNumber {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите любое число: ");
         int input = scanner.nextInt();
-        if (input  % 2 == 0) {
+        if (input  % 2  > 0) {
             System.out.println("Число четное.");
+        }
+        else if (input == 0){
+            System.out.println(" Вы ввели 0.");
         }
         else {
             System.out.println("Число не четное.");
--- Seasons/src/Seasons.java
+++ Seasons/src/Seasons.java
@@ -6,11 +6,15 @@ public class Seasons {
         System.out.println("Введите номер месяца: ");
         int input = scanner.nextInt();
         switch (input) {
-            case 12, 1, 2 -> System.out.println("Зима.");
-            case 3, 4, 5 -> System.out.println("Весна.");
-            case 6, 7, 8 -> System.out.println("Лето.");
-            case 9, 10, 11 -> System.out.println("Осень.");
-            default -> System.out.println("Error.");
+            case 12, 1, 2 : System.out.println("Зима.");
+            break;
+            case 3, 4, 5 : System.out.println("Весна.");
+            break;
+            case 6, 7, 8 : System.out.println("Лето.");
+            break;
+            case 9, 10, 11 :  System.out.println("Осень.");
+            break;
+            default : System.out.println("Error.");
         }
     }
 }
